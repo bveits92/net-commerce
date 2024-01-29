807633e8529a69dabc6a8d00e13f34eae172dc72
Boris <b.veits92@gmail.com>
2024-01-29 14:25:48 -0700

    Initial setup.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace net_commerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
